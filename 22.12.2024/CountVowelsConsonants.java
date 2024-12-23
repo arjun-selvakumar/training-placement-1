a75599ce0a63e32033a6f0ca28dc957bfbfeab22
juno <154307342+arjun-selvakumar@users.noreply.github.com>
2024-12-23 21:36:34 +0530

    Create CountVowelsConsonants.java

---
+++ 22.12.2024/CountVowelsConsonants.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+        int vowels = 0, consonants = 0;
+
+        for (char ch : str.toCharArray()) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else if (Character.isLetter(ch)) {
+                consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
