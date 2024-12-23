4b817b69336ee8a86312c9267c6b247501a8c530
juno <154307342+arjun-selvakumar@users.noreply.github.com>
2024-12-23 21:32:50 +0530

    Create CompoundInterest.java

---
+++ 22.12.2024/CompoundInterest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter principal amount: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter annual interest rate (in %): ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter time (in years): ");
+        int time = sc.nextInt();
+
+        System.out.print("Enter number of times interest applied per year: ");
+        int n = sc.nextInt();
+
+        double amount = principal * Math.pow((1 + (rate / (n * 100))), n * time);
+        double compoundInterest = amount - principal;
+
+        System.out.println("Compound Interest: " + compoundInterest);
+    }
+}
